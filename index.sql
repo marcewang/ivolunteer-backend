22088482f78d6ae75cafbd6936350593e1e380c5
Marcela Wang <marcelawang7@gmail.com>
2019-05-28 15:33:58 -0600

    server finished

--- index.sql
+++ index.sql
@@ -13,12 +13,17 @@ CREATE TABLE user_login (
 
 CREATE TABLE volunteer_opportunities(
     id serial PRIMARY KEY,
-    co_name VARCHAR(100) NOT NULL,
     co_logo VARCHAR(300) NOT NULL,
-    vol_description VARCHAR(2000) NOT NULL,
-    vol_time TIME NOT NULL,
     vol_date DATE NOT NULL ,
-    location VARCHAR(100) NOT NULL
+    vol_start_time TIME NOT NULL,
+    vol_end_time TIME NOT NULL,
+    vol_location VARCHAR(300) NOT NULL,
+    vol_city VARCHAR(100) NOT NULL,
+    co_phone VARCHAR(100) NOT NULL,
+    co_email VARCHAR(100) NOT NULL,
+    co_website VARCHAR(100) NOT NULL,
+    vol_title VARCHAR(200) NOT NULL,
+    vol_description VARCHAR(4000) NOT NULL
 );
 
 CREATE TABLE user_opportunities(
@@ -27,8 +32,69 @@ CREATE TABLE user_opportunities(
     opportunity_id INTEGER NOT NULL
 );
 
-INSERT INTO volunteer_opportunities (co_name,co_logo,vol_description,vol_time,vol_date,location)
-VALUES ('Salvation Army','https://upload.wikimedia.org/wikipedia/en/thumb/c/c4/The_Salvation_Army.svg/200px-The_Salvation_Army.svg.png','Looking for someone to help set up a booth to receive donations', '10:00:00','2019-12-2', 'Calgary')
+INSERT INTO volunteer_opportunities (co_logo, vol_date,vol_start_time,vol_end_time,vol_location, vol_city,co_phone, co_email,co_website,vol_title,vol_description)
+VALUES ('https://www.propellus.org/content/propellus/volunteer/organization-profile/logo/Community-Future%20logo-variants/160w_thumb/Community-Future%20logo.JPG',
+'13-06-2019',
+'10:00:00',
+'15:00:00',
+'1111 - 11 Avenue SW, Calgary, AB, T2R 0G5',
+'Calgary',
+'403-290-5753',
+'volunteer@ccisab.ca', 
+'https://www.ccisab.ca', 
+'Calgary Catholic Immigration Society',
+'Calgary Catholic Immigration Society is a non-profit organization which provides settlement and integration services to all immigrants and refugees in Southern Alberta.
 
-INSERT INTO volunteer_opportunities (co_name,co_logo,vol_description,vol_time,vol_date,location)
-VALUES ('Calgary Public Library','https://presspage-production-content.s3.amazonaws.com/uploads1485/500_calgarypubliclibrarylogogreytransparentbackground-313723.png?x=1544738883211','Looking for someone to greet library vistitors', '10:00:00','2019-12-2', 'Calgary')
+We are a community leader with over 35 years of solid experience in the design and delivery of comprehensive resettlement and integration services to refugees and immigrants. We pride ourselves in delivering these services through a dynamic multicultural, multilingual and multidisciplinary team of professionals, who collectively speak over 60 languages. This diversity enables us to provide our clients and the community with optimum assistance. We have over 1,500 volunteers who devote their time to helping newcomers.
+
+We offer a wide variety of specialized services that were designed to aid and enhance the integration process.
+
+Experience the world without leaving Calgary. Volunteer with immigrant and refugee families today!
+
+Are you interested in interacting with people from different cultures? Do you want to make a difference in newcomers’ lives by supporting their transition to Calgary? Volunteers provide practical and social support to new immigrants and refugees, helping them to learn about Canadian culture, navigate transportation, health-care systems, find resources, practice English, and feel less isolated through participation in social activities. 
+
+Each volunteer or group of volunteers is connected with an individual or family based on the volunteer’s preferences and the client’s needs. Factors to be considered include: volunteer’s skills, background, hobbies, and location in Calgary. Volunteers are expected to visit their assigned families once a week (for 1-4 hours), for 6 months to 1 year, in order to develop a trusting and supportive relationship.
+
+All applicants are subjected to a screening process, and training will be provided on cross-cultural interaction, settlement processes, and practical mentorship support. Ongoing supervision, guidance, and professional development opportunities will be provided.
+
+The Community Connections for Newcomers Program (CCN) is part of the comprehensive support CCIS provides to new immigrants.
+
+If you have any questions or would like to apply, please contact Marcella Ducasses at 403 262 2013 or mducasses@ccisab.ca.' )
+
+
+INSERT INTO volunteer_opportunities (co_logo, vol_date,vol_start_time,vol_end_time,vol_location, vol_city,co_phone, co_email,co_website,vol_title,vol_description)
+VALUES ('https://www.propellus.org/content/propellus/volunteer/organization-profile/logo/1.%20CTH%20Main%20Logo-variants/160w_thumb/1.%20CTH%20Main%20Logo.jpg',
+'2019-06-01',
+'11:00:00',
+'13:00:00',
+'3507 A 17 Avenue SW , Calgary , AB, T3E 0B6',
+'Cochrane',
+'403-543-0550',
+'volunteer@closertohome.com',
+'www.closertohome.com',
+'Volunteer Writer',
+'This is a flexible opportunity for a volunteer writer to contribute to the community. Closer to Home is currently looking for writers to promote CTH’s programs/services, agency events, and stakeholder profiles through CTH print and digital publications, social media and website. The Writer represents the organization on a per project basis. Examples include: interviewing clients and/or staff for case stories, writing articles and blog posts, researching and writing social media posts, and more.
+
+ABOUT CLOSER TO HOME: 
+
+Our Mission: Empowering Families to Stay Together  Our Vision: Closer to Home is committed to contributing to a future where every child will belong to a family and feel valued and secure. Through the use of evidence-based practices and innovative solutions, CTH will strive to preserve, reunify and build stronger families who can care for their children and contribute meaningfully in their community. CTH will provide a broad array of strength-based and family-centered services that teach, coach and support families to create new possibilities and achieve better futures together.  Principles: Diversity Practices that accept and promote individual cultures, traditions, spiritual beliefs and lifestyles.  Services that recognize the complexity of social issues related to the trauma experienced by Indigenous communities and promote Indigenous values, health, wellness, and cultural connections.  Individualized Approach Interventions developed to preserve the family, in partnership with the child and family, are strength-based, inclusive, and solution focused.  Effective Services Integrated quality assurance systems provide the necessary feedback to improve services and facilitate continuous quality improvement that results in better outcomes for children and families.  Outcome-Focused Programs Meaningful outcomes are identified, measured, and achieved with integrity, excellence, and accountability.  Humane Practices Interactions with children and families are respectful, dignifying and positive. Interventions are trauma-informed and focused on family preservation. Children are safe, their opinions are valued and their choices respected.  Client Satisfaction Services solicit the opinion of all participants and stakeholders in a systematic manner, and are responsive to identified needs and concerns.  Collaborative and Inclusive The Organization participates in partnerships that offer benefit to, and reduce duplication of services for children and families.
+')
+
+
+***************************************************************************************************************************
+CREATE TABLE co_login (
+    id serial PRIMARY KEY,
+    co_email text UNIQUE NOT NULL,
+    co_pass VARCHAR(300) NOT NULL
+);
+
+CREATE TABLE companies(
+    id serial PRIMARY KEY,
+    co_name VARCHAR(200) NOT NULL, 
+    co_email VARCHAR(100) NOT NULL,
+    co_phone VARCHAR(100) NOT NULL,
+    co_logo VARCHAR(300) NOT NULL,
+    co_location VARCHAR(300) NOT NULL,
+    co_city VARCHAR(100) NOT NULL,
+    co_website VARCHAR(100) NOT NULL
+);
--- server.js
+++ server.js
@@ -26,8 +26,70 @@ app.get('/', (req, res) => {
     res.send('hello')
 })
 
+app.get('/cities', (req, res) => {
+    db.select('vol_city').count('*')
+        .from('volunteer_opportunities')
+        .groupBy('vol_city')
+        .then(results => {
+            res.json(results)
+        }).catch(err => res.status(400).json(err))
+
+})
+
+app.get('/dates/:city', (req, res) => {
+    const { city } = req.params
+    db.select('vol_date')
+        .from('volunteer_opportunities')
+        .where('vol_city', city)
+        .groupBy('vol_date')
+        .then(dates => {
+            res.json(dates)
+            console.log(dates)
+        })
+})
+
+app.get('/opportunities/:city/:date', (req, res) => {
+    const { city, date } = req.params
+    db.select('*')
+        .from('volunteer_opportunities')
+        .where(!date === "alldates" ? {
+            vol_city: city,
+            vol_date: date // date returns weird 
+        } : { vol_city: city })
+        .then(opportunities => {
+            res.json(opportunities)
+        })
+}
+)
+
+app.get('/myaccount/:email' , (req,res)=> {
+    const {email} = req.params
+    db('users')
+    .join('user_opportunities', 'users.id','user_opportunities.volunteer_id')
+    .join('volunteer_opportunities', 'opportunity_id','volunteer_opportunities.id')
+    .returning('*')
+    .where('email', email)
+    .then(accountdetails => {
+        res.json(accountdetails)
+    })
+    .then(console.log)
+    .catch(err => res.status(400).json(err))
+})
+
+app.get('/user/:email' , (req,res)=> {
+    const {email} = req.params
+    db.select('*')
+    .from('users')
+    .where('email', email)
+    .then(userInfo =>{res.json(userInfo) })
+    .catch(err => res.status(400).json(err))
+})
+
 app.post('/register', (req, res) => {
     const { name, email, phone, password } = req.body
+    if (!email || !name || !password || !phone) {
+        return res.status(400).json('incorrect form submission') //need to return otherwise the rest of the stuff will still run.. so the user is still registered
+    }
     let hash = bcrypt.hashSync(password, 10)
     db.transaction(trx => {
         trx.insert({
@@ -48,9 +110,8 @@ app.post('/register', (req, res) => {
                     })
                     .then(trx.commit)
                     .then(trx.rollback)
-            })
-    }).catch(err => res.status(400).json('unable to register'))
-
+            }).catch(err => res.status(400).json(err.detail))
+    })
 })
 
 app.post('/signin', (req, res) => {
@@ -63,27 +124,38 @@ app.post('/signin', (req, res) => {
             if (isValid) {
                 res.json(login[0].email)
             } else {
-                res.status(400).json('Incorrect password')
+                res.status(400).json("error")
             }
         }).catch(err => {
-            res.status(400).json('Incorrect email')
+            res.status(400).json("error")
         })
 })
 
-app.get('/opportunities', (req, res) => {
-    const { location, date } = req.body
-    db.select('*')
-        .from('volunteer_opportunities')
-        .where({
-            location: location,
-            vol_date: date // date returns weird 
-        })
-        .then(opportunities => {
-            res.json(opportunities)
-        })
+app.post('/signup',(req,res)=> {
+    const {email,id} = req.body
+    db.transaction(trx =>{
+        trx.select('id')
+        .from('users')
+        .where('email', email)
+        .then(userId => {
+            return trx.insert(
+                {
+                    volunteer_id: userId[0].id,
+                    opportunity_id: id                   
+                }
+            )
+            .into('user_opportunities')
+            .returning('*')
+            .then(user_opportunity => {
+                res.json(user_opportunity)
+            })
+            .then(trx.commit)
+            .then(trx.rollback)
+        }).catch(err => res.status(400).json(err))
+    })
 })
 
-app.get('/description', (req, res) => {
+app.post('/description', (req, res) => {
     const { id } = req.body
     db.select('*')
         .from('volunteer_opportunities')
@@ -94,6 +166,99 @@ app.get('/description', (req, res) => {
             res.json(opportunity[0])
         })
 
+})
+//--------------------------------------------------------------------------------------------------------------------
+//COMPANIES ROUTES
+//---------------------------------------------------------------------------------------------------------------------
+app.post('/registerco', (req,res)=>{
+    const { name, email, password, phone, logo, location, city, website } = req.body
+    if (!name || !email || !password || !phone || !logo || !location || !city || !website) {
+        return res.status(400).json('incorrect form submission') //need to return otherwise the rest of the stuff will still run.. so the user is still registered
+    }
+    let hash = bcrypt.hashSync(password, 10)
+    db.transaction(trx => {
+        trx.insert({
+            co_email: email, 
+            co_pass: hash
+        })
+            .into('co_login')
+            .returning('co_email')
+            .then(loginEmail => {
+                return trx('companies').insert({
+                    co_name: name, 
+                    co_email: loginEmail[0],
+                    co_phone: phone,
+                    co_logo: logo,
+                    co_location: location,
+                    co_city: city, 
+                    co_website: website
+                })
+                    .returning('*')
+                    .then(user => {
+                        res.json(user[0])
+                    })
+                    .then(trx.commit)
+                    .then(trx.rollback)
+            }).catch(err => res.status(400).json(err.detail))
+    })
+})
+
+
+app.post('/signinco', (req, res) => {
+    const { email, password } = req.body
+    db.select('co_email', 'co_pass')
+        .from('co_login')
+        .where('co_email', email)
+        .then(login => {
+            let isValid = bcrypt.compareSync(password, login[0].co_pass)
+            if (isValid) {
+                res.json(login[0].co_email)
+            } else {
+                res.status(400).json('error')
+            }
+        }).catch(err => {
+            res.status(400).json('error')
+        })
+})
+
+
+app.get('/userco/:email' , (req,res)=> {
+    const {email} = req.params
+    db.select('*')
+    .from('companies')
+    .where('co_email', email)
+    .then(userInfo =>{res.json(userInfo) })
+    .catch(err => res.status(400).json(err))
+})
+
+app.get('/accountco/:email' , (req,res)=> {
+    const {email} = req.params
+    db.select('*')
+    .from('volunteer_opportunities')
+    .where('co_email', email)
+    .then(userInfo =>{res.send(userInfo) })
+    .catch(err => res.status(400).json(err))
+})
+
+app.post('/postopportunity', (req,res) =>{
+    const{logo, date, start_time, end_time , location, city, phone,email, website, title, description }= req.body
+    db('volunteer_opportunities')
+    .insert({
+        co_logo: logo,
+        vol_date: date,
+        vol_start_time:start_time,
+        vol_end_time: end_time,
+        vol_location:location,
+        vol_city: city,
+        co_phone:phone,
+        co_email:email,
+        co_website: website,
+        vol_title: title,
+        vol_description: description
+    })
+    .returning('*')
+    .then(opportunity => res.json(opportunity))
+    .catch(err => res.status(400).json(err))
 })
 
 
